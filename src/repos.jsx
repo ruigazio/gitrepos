b81068b2d35c6ba4f8c763f1b078e2759a4eb23a
Rui Gazio <rgazio@gmail.com>
2017-07-31 20:03:54 +0100

    working prototype. No styling. No button.

--- src/repos.jsx
+++ src/repos.jsx
@@ -1,6 +1,79 @@
 import React from 'react'
 import Api from './api'
 
+
+function Repo({repo}) {
+  const {name, desc, lang, stars, forks} = repo
+  return (
+    <li className='repo'>
+      <div className='name'>{name}</div>
+      <div className='desc'>{desc}</div>
+      <div className='lang'>{lang}</div>
+      <div className='stars'>{stars}</div>
+      <div className='forks'>{forks}</div>
+    </li>
+  )
+}
+
+/****************** START Repo box branches *************************/
+function UserNotFound({username}) {
+  return (
+    <div className='not-valid'>
+      <h4> The user {username} was not found</h4>
+    </div>
+  )
+}
+
+function RepoLoading() {
+  return (
+    <div className='loading'>
+      <h4> Loading </h4>
+    </div>
+  )
+}
+
+function RepoHelp() {
+  return (
+    <div className='no-username'>
+      <h4> Type the github username above and press ENTER </h4>
+    </div>
+  )
+}
+
+function EmptyRepos({username}) {
+  return (
+    <div className='no-repos'>
+      <h4> No repos for user {username} </h4>
+    </div>
+  )
+}
+
+function RepoList(props) {
+  return (
+    <div>
+      <h3>User {props.username} </h3>
+      <ul className="repo-list">
+        {
+          props.repos.map( repo => ( <Repo key={repo.name} repo={repo} /> ) )
+        }
+      </ul>
+    </div>
+  )
+}
+/****************** END Repo box branches *************************/
+
+function RepoContent(props) {
+  return props.username ?
+    props.loading ?
+      <RepoLoading />
+      : props.valid ?
+          props.repos ?
+            <RepoList {...props} />
+            : <EmptyRepos {...props} />
+          : <UserNotFound {...props} />
+    : <RepoHelp />
+}
+
 class UsernameInput extends React.Component {
   constructor() {
     super()
@@ -36,52 +109,10 @@ class UsernameInput extends React.Component {
   }
 }
 
-function UserNotFound({username}) {
-  return (
-    <h4> The user {username} was not found</h4>
-  )
-}
-
-
-function EmptyRepos({username}) {
-  return (
-    <h4> No repos for user {username} </h4>
-  )
-}
-
-function RepoList(props) {
-  function Repo(props) {
-    return (
-        <li className="repo">
-          {props.repo.name}
-        </li>
-    )
-  }
-
-  return (
-    <div>
-      <h3>User {props.username} </h3>
-      <ul className="repo-list">
-        {
-          props.repos.map( repo => ( <Repo key={repo.name} repo={repo} /> ) )
-        }
-      </ul>
-    </div>
-  )
-}
-
-function RepoContent(props) {
-  return props.valid ?
-    props.repos ?
-      ( <RepoList {...props} /> )
-      : ( <EmptyRepos {...props} /> )
-    : <UserNotFound {...props} />
-}
-
 class Repos extends React.Component {
   constructor() {
     super()
-    this.state = {username: null, repos: null, valid: null}
+    this.state = {username: null, repos: null, valid: null, loading: false}
   }
 
   render() {
@@ -95,10 +126,13 @@ class Repos extends React.Component {
 
   onUserChange(username) {
     this.setState({username})
-    this.loadRepos(username)
+    if (username) {
+      this.loadRepos(username)
+    }
   }
 
   async loadRepos(username) {
+    this.setState({loading: true})
     const {ok, response} = await Api.fetchReposP(username)
     let repos, valid = false
     if (ok) {
@@ -106,9 +140,8 @@ class Repos extends React.Component {
       valid = true
     }
     console.log('setting state', repos)
-    this.setState({repos, valid})
+    this.setState({repos, valid, loading: false})
   }
 }
-
 
 export default Repos
